8326e6f37d4b66a7af21bf1b54b27495df5422da
Anteccq <39259186+Anteccq@users.noreply.github.com>
2023-07-01 17:04:55 +0900

    Initial commit

---
+++ SimpleNotificator/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using Zx;
+
+if (ToastNotificationManagerCompat.WasCurrentProcessToastActivated())
+{
+    var tcs = new CancellationTokenSource();
+    ToastNotificationManagerCompat.OnActivated += async e =>
+    {
+        Console.WriteLine($"start {e.Argument}");
+        await $"start {e.Argument}";
+        tcs.Cancel();
+    };
+    while (!tcs.IsCancellationRequested){ }
+}
+
+ConsoleApp.Run<SimpleNotificator.SimpleNotificator>(args);
---
+++ SimpleNotificator/SimpleNotificator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace SimpleNotificator;
+
+public class SimpleNotificator : ConsoleAppBase
+{
+    [RootCommand]
+    public void Execute([Option("t")] string title, [Option("c")] string content, [Option("a")] string action)
+    {
+        new ToastContentBuilder()
+            .SetToastScenario(ToastScenario.Reminder)
+            .AddText(title)
+            .AddText(content)
+            .AddArgument(action)
+            .AddAttributionText("Simple Notificator 🎆")
+            .Show();
+    }
+}
